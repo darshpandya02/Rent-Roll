008dcc1ac9b1050aeb0ac39753c3c609b7ad2be0
darshpandya02 <pandya.dar@northeastern.edu>
2024-04-15 22:25:46 -0400

    final project

---
+++ Server/Db/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+function connectDb() {
+  mongoose.connect("MONGODB_URI_REMOVED", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  });
+  const connection = mongoose.connection;
+  connection.on("connected", () => {
+    console.log("connection successfull");
+  });
+  connection.on("error", () => {
+    console.log("connection failed");
+  });
+}
+
+connectDb();
+module.exports = mongoose;
